334f730e8af184e7ef270bcf498ac974ef7fb81d
Clever Creative <107514372+clevercreativeofficial@users.noreply.github.com>
2026-02-01 09:51:04 +0100

    Update Aside.tsx

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -71,29 +71,7 @@ const Aside = () => {
             </div>
 
             {/* Developer Profile */}
-            <div className='space-y-3'>
-                <h3 className='text-lg font-semibold text-neutral-800 dark:text-white'>Developer</h3>
-                <div className='flex items-center space-x-4'>
-                    <img 
-                        src="https://ik.imagekit.io/qq6z28mp5/profile_sm.jpg" 
-                        alt="Developer" 
-                        width={60} 
-                        height={60} 
-                        className='rounded-full border-2 border-neutral-300 dark:border-neutral-700'
-                    />
-                    <div>
-                        <p className='font-medium text-neutral-800 dark:text-white'>Clever Creative</p>
-                        <div className='flex space-x-3 mt-1'>
-                            <a href="https://github.com/clvrdesign" className='text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-white transition-colors'>
-                                <Github size={18} />
-                            </a>
-                            <a href="#" className='text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-white transition-colors'>
-                                <Dribbble size={18} />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     )
 }
